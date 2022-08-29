8a319abead5dd4d20fb4a743f2806e606385a12c
Paweł Putra <pawel.putra@scylladb.com>
2022-08-29 13:45:39 +0200

    gocql: add marshal.go

---
+++ gocql/types.go
@@ -0,0 +1,28 @@
+package gocql
+
+type unsetColumn struct{}
+
+// UnsetValue represents a value used in a query binding that will be ignored by Cassandra.
+//
+// By setting a field to the unset value Cassandra will ignore the write completely.
+// The main advantage is the ability to keep the same prepared statement even when you don't
+// want to update some fields, where before you needed to make another prepared statement.
+//
+// UnsetValue is only available when using the version 4 of the protocol.
+var UnsetValue = unsetColumn{}
+
+const (
+	protoDirectionMask = 0x80
+	protoVersionMask   = 0x7F
+	protoVersion1      = 0x01
+	protoVersion2      = 0x02
+	protoVersion3      = 0x03
+	protoVersion4      = 0x04
+	protoVersion5      = 0x05
+)
+
+type Duration struct {
+	Months      int32
+	Days        int32
+	Nanoseconds int64
+}
