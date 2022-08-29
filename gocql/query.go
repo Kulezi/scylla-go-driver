f34647e8e14963cee7de0d957b14fb5a15c6158d
Paweł Putra <pawel.putra@scylladb.com>
2022-08-29 15:50:24 +0200

    gocql: query, add missing method signatures

--- gocql/query.go
+++ gocql/query.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/gocql/gocql"
 	"github.com/kulezi/scylla-go-driver"
 )
 
@@ -56,4 +57,71 @@ func (q *Query) Release() {
 func (q *Query) WithContext(ctx context.Context) *Query {
 	q.ctx = ctx
 	return q
+}
+
+func (q *Query) Consistency(c gocql.Consistency) *Query {
+	panic("unimplemented")
+}
+
+// CustomPayload sets the custom payload level for this query.
+func (q *Query) CustomPayload(customPayload map[string][]byte) *Query {
+	panic("unimplemented")
+}
+
+// Trace enables tracing of this query. Look at the documentation of the
+// Tracer interface to learn more about tracing.
+func (q *Query) Trace(trace gocql.Tracer) *Query {
+	panic("unimplemented")
+}
+
+// Observer enables query-level observer on this query.
+// The provided observer will be called every time this query is executed.
+func (q *Query) Observer(observer gocql.QueryObserver) *Query {
+	panic("unimplemented")
+}
+
+func (q *Query) PageSize(n int) *Query {
+	q.query.SetPageSize(int32(n))
+	return q
+}
+
+func (q *Query) DefaultTimestamp(enable bool) *Query {
+	panic("unimplemented")
+}
+
+func (q *Query) WithTimestamp(timestamp int64) *Query {
+	panic("unimplemented")
+}
+
+func (q *Query) RoutingKey(routingKey []byte) *Query {
+	panic("unimplemented")
+}
+
+func (q *Query) Prefetch(p float64) *Query {
+	panic("unimplemented")
+}
+
+func (q *Query) RetryPolicy(r gocql.RetryPolicy) *Query {
+	panic("unimplemented")
+}
+
+func (q *Query) SetSpeculativeExecutionPolicy(sp gocql.SpeculativeExecutionPolicy) *Query {
+	panic("unimplemented")
+}
+
+func (q *Query) Idempotent(value bool) *Query {
+	panic("unimplemented")
+	// q.query.SetIdempotent(value)
+}
+
+func (q *Query) SerialConsistency(cons gocql.SerialConsistency) *Queryx {
+	panic("unimplemented")
+}
+
+func (q *Query) PageState(state []byte) *Query {
+	panic("unimplemented")
+}
+
+func (q *Query) NoSkipMetadata() *Query {
+	panic("unimplemented")
 }
