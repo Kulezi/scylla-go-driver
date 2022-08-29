738970aa372893476fba17896a5181c22e73830b
Paweł Putra <pawel.putra@scylladb.com>
2022-08-29 13:27:01 +0200

    split gocql into more files

--- gocql/session.go
+++ gocql/session.go
@@ -2,9 +2,8 @@ package gocql
 
 import (
 	"context"
-	"fmt"
 
-	scylla "github.com/kulezi/scylla-go-driver"
+	"github.com/kulezi/scylla-go-driver"
 )
 
 type Consistency = scylla.Consistency
@@ -39,68 +38,4 @@ func (s *Session) Query(stmt string, values ...interface{}) *Query {
 
 func (s *Session) Close() {
 	s.session.Close()
-}
-
-type Query struct {
-	query scylla.Query
-	err   error
-}
-
-func (q *Query) Bind(values ...interface{}) *Query {
-	for i, v := range values {
-		q.query.BindAny(i, v)
-	}
-
-	return q
-}
-
-func (q *Query) Exec() error {
-	_, err := q.query.Exec(context.Background())
-	return err
-}
-
-func (q *Query) Scan(values ...interface{}) error {
-	res, err := q.query.Exec(context.Background())
-	if err != nil {
-		return err
-	}
-
-	if len(res.Rows[0]) != len(values) {
-		return fmt.Errorf("column count mismatch expected %d, got %d", len(values), len(res.Rows))
-	}
-
-	for i, v := range res.Rows[0] {
-		if err := v.Unmarshal(values[i]); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func (q *Query) Iter() Iter {
-	return Iter{q.query.Iter(context.Background())}
-}
-
-type Scanner interface {
-	// Next advances the row pointer to point at the next row, the row is valid until
-	// the next call of Next. It returns true if there is a row which is available to be
-	// scanned into with Scan.
-	// Next must be called before every call to Scan.
-	Next() bool
-
-	// Scan copies the current row's columns into dest. If the length of dest does not equal
-	// the number of columns returned in the row an error is returned. If an error is encountered
-	// when unmarshalling a column into the value in dest an error is returned and the row is invalidated
-	// until the next call to Next.
-	// Next must be called before calling Scan, if it is not an error is returned.
-	Scan(...interface{}) error
-
-	// Err returns the if there was one during iteration that resulted in iteration being unable to complete.
-	// Err will also release resources held by the iterator, the Scanner should not used after being called.
-	Err() error
-}
-
-type Iter struct {
-	it scylla.Iter
 }
