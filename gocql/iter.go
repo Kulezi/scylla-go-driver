73a2b65c21d15b56c276c7312465cc005d0d4d80
Paweł Putra <pawel.putra@scylladb.com>
2022-09-01 10:43:19 +0200

    gocql: add iter.MapScan()

--- gocql/iter.go
+++ gocql/iter.go
@@ -2,6 +2,7 @@ package gocql
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/kulezi/scylla-go-driver"
 	"github.com/kulezi/scylla-go-driver/frame"
@@ -64,4 +65,79 @@ func (it *Iter) Scan(dest ...interface{}) bool {
 
 func (it *Iter) PageState() []byte {
 	return it.it.PageState()
+}
+
+type RowData struct {
+	Columns []string
+	Values  []interface{}
+}
+
+// TupeColumnName will return the column name of a tuple value in a column named
+// c at index n. It should be used if a specific element within a tuple is needed
+// to be extracted from a map returned from SliceMap or MapScan.
+func TupleColumnName(c string, n int) string {
+	return fmt.Sprintf("%s[%d]", c, n)
+}
+
+func (iter *Iter) RowData() (RowData, error) {
+	if iter.err != nil {
+		return RowData{}, iter.err
+	}
+
+	columns := make([]string, 0, len(iter.Columns()))
+	values := make([]interface{}, 0, len(iter.Columns()))
+
+	for _, column := range iter.Columns() {
+		if c, ok := column.TypeInfo.(TupleTypeInfo); !ok {
+			val := column.TypeInfo.New()
+			columns = append(columns, column.Name)
+			values = append(values, val)
+		} else {
+			for i, elem := range c.Elems {
+				columns = append(columns, TupleColumnName(column.Name, i))
+				values = append(values, elem.New())
+			}
+		}
+	}
+
+	return RowData{
+		Columns: columns,
+		Values:  values,
+	}, nil
+}
+
+func (it *Iter) MapScan(m map[string]interface{}) bool {
+	if it.err != nil {
+		return false
+	}
+
+	// Not checking for the error because we just did
+	rowData, _ := it.RowData()
+
+	for i, col := range rowData.Columns {
+		if dest, ok := m[col]; ok {
+			rowData.Values[i] = dest
+		}
+	}
+	if it.Scan(rowData.Values...) {
+		rowData.rowMap(m)
+		return true
+	}
+	return false
+}
+
+func dereference(i interface{}) interface{} {
+	return reflect.Indirect(reflect.ValueOf(i)).Interface()
+}
+func (r *RowData) rowMap(m map[string]interface{}) {
+	for i, column := range r.Columns {
+		val := dereference(r.Values[i])
+		if valVal := reflect.ValueOf(val); valVal.Kind() == reflect.Slice {
+			valCopy := reflect.MakeSlice(valVal.Type(), valVal.Len(), valVal.Cap())
+			reflect.Copy(valCopy, valVal)
+			m[column] = valCopy.Interface()
+		} else {
+			m[column] = val
+		}
+	}
 }
